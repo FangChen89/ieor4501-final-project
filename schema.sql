cd44e868cc666e3d4744ea34259ea945d925c17d
FangChen89 <fc2721@columbia.edu>
2023-04-30 23:59:18 -0400

    Update schema.sql of 4 sql statements

--- schema.sql
+++ schema.sql
@@ -1,1 +1,36 @@
--- Write your tables' schemas in this file
+
+CREATE TABLE IF NOT EXISTS hourly_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME,
+    hourly_precipitation FLOAT,
+    hourly_windspeed FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS daily_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME,
+    daily_average_windspeed FLOAT,
+    sunrise DATETIME,
+    sunset DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS taxi_trips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME,
+    pickup_longitude FLOAT,
+    pickup_latitude FLOAT,
+    dropoff_longitude FLOAT,
+    dropoff_latitude FLOAT,
+    tip_amount FLOAT,
+    distance FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS uber_trips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME,
+    pickup_longitude FLOAT,
+    pickup_latitude FLOAT,
+    dropoff_longitude FLOAT,
+    dropoff_latitude FLOAT,
+    distance FLOAT
+);
